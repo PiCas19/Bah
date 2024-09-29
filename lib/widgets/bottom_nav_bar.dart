1f52ea221708cdaab92fe7516a905473d5308081
Pierpaolo Casati <pierpaolo.casati@student.supsi.ch>
2024-09-29 15:18:00 +0200

    modified: navbar bottom system

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -1,3 +1,5 @@
+import '../pages/profile_page.dart';
+import '../pages/explore_page.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavBar extends StatefulWidget {
@@ -10,6 +12,14 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar> {
   int _selectedIndex = 0;
 
+  static const List<Widget> _pages = <Widget>[
+    ExplorePage(),
+    Text("Preferiti"),
+    Text("Itinerario"),
+    Text("Messaggi"),
+    ProfilePage()
+  ];
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -18,35 +28,38 @@ class _BottomNavBarState extends State<BottomNavBar> {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: Colors.white, // Set background color to white
-      selectedItemColor: Colors.blue, // Color for selected item
-      unselectedItemColor: Colors.grey, // Color for unselected items
-      currentIndex: _selectedIndex,
-      onTap: _onItemTapped,
-      items: [
-        BottomNavigationBarItem(
-          icon: _buildIcon(Icons.search, 0),
-          label: 'Esplora',
-        ),
-        BottomNavigationBarItem(
-          icon: _buildIcon(Icons.favorite_outline, 1), // Outline icon for favorite
-          label: 'Preferiti',
-        ),
-        BottomNavigationBarItem(
-          icon: _buildIcon(Icons.directions_boat, 2), // Boat icon for 'Itinerario'
-          label: 'Itinerario',
-        ),
-        BottomNavigationBarItem(
-          icon: _buildIcon(Icons.message_outlined, 3), // Outline message icon
-          label: 'Messaggi',
-        ),
-        BottomNavigationBarItem(
-          icon: _buildIcon(Icons.person_outline, 4), // Outline profile icon
-          label: 'Profilo',
-        ),
-      ],
+    return Scaffold(
+      body: _pages[_selectedIndex],
+        bottomNavigationBar:BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.white, // Set background color to white
+          selectedItemColor: Colors.blue, // Color for selected item
+          unselectedItemColor: Colors.grey, // Color for unselected items
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: [
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.search, 0),
+              label: 'Esplora',
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.favorite_outline, 1), // Outline icon for favorite
+              label: 'Preferiti',
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.directions_boat, 2), // Boat icon for 'Itinerario'
+              label: 'Itinerario',
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.message_outlined, 3), // Outline message icon
+              label: 'Messaggi',
+            ),
+            BottomNavigationBarItem(
+              icon: _buildIcon(Icons.person_outline, 4), // Outline profile icon
+              label: 'Profilo',
+            ),
+          ],
+        )
     );
   }
 
